a83a787bdf106edcbc35f097ea8e7b0432fc0467
Chengxun Lee <24319042+bclswl0827@users.noreply.github.com>
2023-05-04 23:15:03 +0800

    Less FRAME_PADDING length

--- include/config.hpp
+++ include/config.hpp
@@ -6,19 +6,17 @@
 #define SERIAL_BAUD 19200
 // ADS1115 I2C 地址
 #define ADC_ADDRESS 0x48
-// ADS1115 测量精度
-#define ADC_PRECISION 0
 // 采样率（单位 Sps）
 #define SAMPLE_RATE 35
+// ADS1115 测量精度（0 为最大增益）
+#define ADC_PRECISION 0
 
 // 数据帧请求字
 const uint8_t FRAME_REQUEST = 0x55;
 // 数据帧起始字节
 const uint8_t FRAME_HEADER[] = {0xAA, 0x55};
 // 数据帧填充字节
-const uint8_t FRAME_PADDING[] = {
-    0x58, 0x58, 0x58, 0x58, 0x58, 0x58, 0x58, 0x58,
-};
+const uint8_t FRAME_PADDING[] = {0x40, 0x40, 0x40};
 
 // 检波器数据帧
 struct SensorData {
