1557671ededee1ae4395f52903993a50c913bf35
Chengxun Lee <24319042+bclswl0827@users.noreply.github.com>
2023-05-04 11:37:14 +0800

    Higher sample rate

--- include/config.hpp
+++ include/config.hpp
@@ -2,12 +2,16 @@
 
 #include <stdint.h>
 
+// 串口波特率
+#define SERIAL_BAUD 19200
 // ADS1115 I2C 地址
 #define ADC_ADDRESS 0x48
 // ADS1115 测量精度
 #define ADC_PRECISION 0
 // 请求字节等待超时
 #define TIMEOUT_MS 100
+// 采样率（单位 Sps）
+#define SAMPLE_RATE 20
 
 // 数据帧填充字节
 const uint8_t FRAME_PADDING = 0x56;
@@ -17,7 +21,7 @@ const uint8_t FRAME_REQUEST[] = {0x55, 0x55};
 const uint8_t FRAME_HEADER[] = {0x55, 0xAA};
 
 // 检波器数据帧
-struct DataFrame {
+struct SensorData {
     float Vertical;
     float EastWest;
     float NorthSouth;
--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,39 @@
 #include <Arduino.h>
-#include <Wire.h>
+
+#define PT_USE_TIMER
+#define PT_USE_SEM
 
 #include "ADS1X15.h"
 #include "config.hpp"
+#include "pt.h"
 
 ADS1115 ads(ADC_ADDRESS);
+SensorData sensorData[SAMPLE_RATE];
+
+static struct pt ThreadSerialListener;
+static struct pt ThreadGeophoneReader;
+
+void setup() {
+    PT_INIT(&ThreadSerialListener);
+    PT_INIT(&ThreadGeophoneReader);
+}
 
 uint8_t isRequest(uint8_t* array, size_t length) {
-    size_t count = 0;
-    uint32_t start = millis();
+    uint64_t start = millis();
+    size_t counter = 0;
 
-    while (millis() - start < TIMEOUT_MS && count < length) {
+    while (millis() - start < TIMEOUT_MS && counter < length) {
         if (Serial.available()) {
             uint8_t byte = Serial.read();
 
-            if (byte == array[count]) {
-                count++;
+            if (byte == array[counter]) {
+                counter++;
             } else {
-                count = 0;
+                counter = 0;
             }
 
-            if (count == length) {
+            if (counter == length) {
+                Serial.flush();
                 return 1;
             }
         }
@@ -29,28 +42,57 @@ uint8_t isRequest(uint8_t* array, size_t length) {
     return 0;
 }
 
-void setup() {
-    Serial.begin(19200);
+static int SerialListener(struct pt* pt) {
+    PT_BEGIN(pt);
+
+    Serial.begin(SERIAL_BAUD);
+    while (1) {
+        if (isRequest((uint8_t*)FRAME_REQUEST, sizeof(FRAME_REQUEST))) {
+            Serial.write(FRAME_HEADER, sizeof(FRAME_HEADER));
+            PT_YIELD(pt);
+            Serial.write((uint8_t*)&sensorData, sizeof(sensorData));
+        } else {
+            Serial.write(FRAME_PADDING);
+        }
+
+        Serial.flush();
+        PT_YIELD(pt);
+    }
+
+    PT_END(pt);
+}
+
+uint16_t getDelay(uint8_t sample) {
+    if (sample == 0) {
+        return 0;
+    }
+
+    return 1000 / sample;
+}
+
+static int GeophoneReader(struct pt* pt) {
+    PT_BEGIN(pt);
+
     ads.begin();
+    while (1) {
+        for (uint8_t i = 0; i < SAMPLE_RATE; i++) {
+            ads.setGain(ADC_PRECISION);
+            float f = ads.toVoltage(1);
+            sensorData[i] = {
+                .Vertical = (float)ads.readADC(0) * f,
+                .EastWest = (float)ads.readADC(1) * f,
+                .NorthSouth = (float)ads.readADC(2) * f,
+            };
+
+            PT_TIMER_DELAY(pt, getDelay(SAMPLE_RATE));
+            PT_YIELD(pt);
+        }
+    }
+
+    PT_END(pt);
 }
 
 void loop() {
-    if (isRequest((uint8_t*)FRAME_REQUEST, sizeof(FRAME_REQUEST))) {
-        ads.setGain(ADC_PRECISION);
-        float f = ads.toVoltage(1);
-        DataFrame dataFrame = {
-            .Vertical = (float)ads.readADC(0) * f,
-            .EastWest = (float)ads.readADC(1) * f,
-            .NorthSouth = (float)ads.readADC(2) * f,
-        };
-
-        Serial.write(FRAME_HEADER, sizeof(FRAME_HEADER));
-        delayMicroseconds(500);
-        Serial.write((uint8_t*)&dataFrame, sizeof(dataFrame));
-    } else {
-        Serial.write(FRAME_PADDING);
-    }
-
-    delayMicroseconds(500);
-    Serial.flush();
+    SerialListener(&ThreadSerialListener);
+    GeophoneReader(&ThreadGeophoneReader);
 }
